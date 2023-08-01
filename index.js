59dae0cbba98ac0388f91d6cd46eeff4e01bf2f9
Ashley Frank <ashley.frank@ufl.edu>
2023-07-31 22:30:14 -0400

    added title and author

--- index.js
+++ index.js
@@ -9,7 +9,25 @@ const pipe = (...fns) => firstArg => fns.reduce((returnValue, fn) => fn(returnVa
 const makeTag = tag => str => `<${tag}>${str}</${tag}>`
 
 // complete this function
-const makePoemHTML = () => {}
+const makePoemHTML = (poem) =>{
+  console.log(poem) 
+  
+  //Make Title Tag
+  const titleTag = makeTag('h2')(poem[0].title)
+
+  //Make Author Tag using pipe
+  const makeEm = makeTag('em')
+  const makeH3 = makeTag('h3')
+  const makeAuthorTag = pipe(makeEm, makeH3)
+  const authorTag = makeAuthorTag(`by ${poem[0].author}`)
+
+  // poem.map()
+
+  var finalString = `${titleTag} ${authorTag}`
+  return finalString
+ 
+
+}
 
 // attach a click event to #get-poem
 getPoemBtn.onclick = async function() {
